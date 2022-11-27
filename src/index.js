53204fc79674f8a2a19f28d731ab119df3e6a2e5
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2022-11-27 18:40:46 -0300

    fix: :bug: fix and create post and get

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-import express from "express";
-import { uuidv4 } from "uuid";
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 app.use(express.json());
 
-const cars = {};
+const cars = [];
 
-app.post("localhost:3000/cars", (request, response) => {
-  const { model, brand, year, color, plate } = request.body;
+app.post('/cars', (request, response) => {
+	const { model, brand, year, color, plate } = request.body;
 
-  cars.push({
-    model,
-    brand,
-    year,
-    color,
-    plate,
-    id: uuidv4(),
-  });
+	const carAlreadyExist = cars.find((car) => car.year === year);
 
-  return response.status(201);
+	if (carAlreadyExist) {
+		return res.status(400).json({ error: 'this car already exist' });
+	}
+
+	cars.push({
+		model,
+		brand,
+		year,
+		color,
+		plate,
+		id: uuidv4(),
+	});
+
+	return response.status(201).send();
 });
 
-app.get("localhost:3000/", (response) => {
-  response.sendStatus("Kenzie Cars");
+app.get('/', (request, response) => {
+	return response.json(cars);
 });
 
-app.listen("localhost:3000");
+app.listen('3000');
